f87575912f590ce0f593a71411e4ee49de7391b6
Hammam Alzilitni <hammam@Hammams-MBP.broadband>
2022-03-22 14:30:39 +0000

    bug fixes

--- Screens/Focus.js
+++ Screens/Focus.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Timer from "../components/Timer";
 
 function Focus(props) {
   return (
     <View>
-      <Text>Focus</Text>
+      <Timer />
     </View>
   );
 }
