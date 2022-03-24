744fd0a1164596ad64bff4262029eb445c1a5106
M4D4R4x <hossein.heydari.scc@hotmail.com>
2022-03-24 10:10:16 +0000

    trackplayer added to body 1

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Image,TouchableOpacity} from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./Screens/Home";
@@ -8,65 +8,91 @@ import Meditation from "./Screens/Meditation";
 import Header from "./components/Header";
 import HomeScreen from "./Screens/HomeScreen";
 import LoginScreen from "./Screens/LoginScreen";
+import TrackPlayer from "./components/TrackPlayer";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-   
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: true }}>
-      <Stack.Screen name="LoginScreen" component={LoginScreen} options={({navigation}) => ({ 
-          title: 'Zenify',
-          headerStyle: {
-            backgroundColor: 'black',
-          },headerLeft: ()=> null,
-          headerTitle: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')}>
-            <Image style={{ width: 30, height: 30 }} source={require("./components/PngItem_4047206.png")} />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={({ navigation }) => ({
+            title: "Zenify",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerLeft: () => null,
+            headerTitle: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("LoginScreen")}
+              >
+                <Image
+                  style={{ width: 30, height: 30 }}
+                  source={require("./components/PngItem_4047206.png")}
+                />
               </TouchableOpacity>
             ),
-          headerTintColor: 'dodgerblue',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        })}/>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} options={({navigation}) => ({ 
-          title: 'Zenify',
-          headerStyle: {
-            backgroundColor: 'black',
-          }, 
-          headerTitle: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-            <Image style={{ width: 30, height: 30 }} source={require("./components/PngItem_4047206.png")} />
-              </TouchableOpacity>
-            ),
-          headerTintColor: 'dodgerblue',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        })}/>
-        <Stack.Screen name="Home" component={Home} options={({navigation}) => ({ 
-          title: 'Zenify',
-          headerStyle: {
-            backgroundColor: 'black',
-          }, headerLeft: ()=> null,
-          // `headerLeft: undefined` should work too
-          // `headerLeft: null` should work but could trigger a TS error
-
-          headerTitle: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-            <Image style={{ width: 30, height: 30 }} source={require("./components/PngItem_4047206.png")} />
-              </TouchableOpacity>
-            ),
-            
-          headerTintColor: 'dodgerblue',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        })}
+            headerTintColor: "dodgerblue",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          })}
         />
-  
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "Zenify",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerTitle: () => (
+              <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                <Image
+                  style={{ width: 30, height: 30 }}
+                  source={require("./components/PngItem_4047206.png")}
+                />
+              </TouchableOpacity>
+            ),
+            headerTintColor: "dodgerblue",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          })}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={({ navigation }) => ({
+            title: "Zenify",
+            headerStyle: {
+              backgroundColor: "black",
+            },
+            headerLeft: () => null,
+            // `headerLeft: undefined` should work too
+            // `headerLeft: null` should work but could trigger a TS error
+
+            headerTitle: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("HomeScreen")}
+              >
+                <Image
+                  style={{ width: 30, height: 30 }}
+                  source={require("./components/PngItem_4047206.png")}
+                />
+              </TouchableOpacity>
+            ),
+
+            headerTintColor: "dodgerblue",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          })}
+        />
+        <Stack.Screen name="TrackPlayer" component={TrackPlayer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/Meditation.js
+++ Screens/Meditation.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import { StyleSheet, View,Text } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Switch, View, Text, TouchableOpacity } from "react-native";
 
-function Meditation(props) {
-    return (
-        <View >
-            <Text>Meditation</Text>
-        </View>
-    );
+function Meditation({ navigation }) {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.switch}>
+        <Text> {isEnabled ? "title1" : "title2"}</Text>
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#ffff"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+      <View>
+        {isEnabled ? (
+          <View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("TrackPlayer")}
+            >
+              <Text>body1</Text>
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View>
+            <Text>body2</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
 }
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       backgroundColor: 'black',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//       color:'white'
-//     },
-//   });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  switch: {
+    alignSelf: "flex-start",
+  },
+});
 export default Meditation;
