07a6db1bfa6dafef77f3ccfb83b9c1cca7c4d78c
blameaimer <blameaimer@gmail.com>
2022-03-28 11:02:58 +0100

    Displayed session data on stats

--- components/DisplayStats.jsx
+++ components/DisplayStats.jsx
@@ -11,11 +11,13 @@ const DisplayStats = () => {
     const userName = auth.currentUser?.displayName;
     const FocusSessionRef = db.ref("users").child(userName).child("Focus");
     const BreakSessionRef = db.ref("users").child(userName).child("Break");
+
     const Breaklistener = BreakSessionRef.on(
       "value",
       (snapshot) => {
         const fetchedSessions = [];
-        snapshot.forEach((session) => {
+
+        snapshot.val().forEach((session) => {
           fetchedSessions.push(session);
         });
         setBreakSessions(fetchedSessions);
@@ -28,7 +30,7 @@ const DisplayStats = () => {
       "value",
       (snapshot) => {
         const fetchedSessions = [];
-        snapshot.forEach((session) => {
+        snapshot.val().forEach((session) => {
           fetchedSessions.push(session);
         });
         setFocusSessions(fetchedSessions);
@@ -46,15 +48,68 @@ const DisplayStats = () => {
 
   const FocusCount = FocusSessions.length;
   const BreakCount = BreakSessions.length;
+  let TotalWorkTime = 0;
+  let TotalBreakTime = 0;
+  FocusSessions.forEach((session) => {
+    TotalWorkTime += session.time;
+  });
+  BreakSessions.forEach((session) => {
+    TotalBreakTime += session.time;
+  });
 
-  FocusSessions.forEach(({ time }) => console.log(time));
   return (
-    <View>
-      <Text>DisplayStats</Text>
+    <View style={styles.container}>
+      <View></View>
+      <View></View>
+      <View></View>
+      <View style={styles.sessions}>
+        <View style={styles.top}>
+          <View style={styles.infobox}>
+            <Text style={styles.text}>Focus Sessions Work Time</Text>
+            <Text style={styles.text}>
+              {FocusCount} {TotalWorkTime}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.bottom}>
+          <View style={styles.infobox}>
+            <Text style={styles.text}>Breaks Break Time </Text>
+            <Text style={styles.text}>
+              {BreakCount} {TotalBreakTime}
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default DisplayStats;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#181818",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "dodgerblue",
+  },
+  sessions: {
+    width: "100%",
+  },
+  infobox: {},
+  top: {
+    backgroundColor: "black",
+    flexDirection: "row",
+  },
+  bottom: {
+    backgroundColor: "pink",
+    flexDirection: "row",
+  },
+});
